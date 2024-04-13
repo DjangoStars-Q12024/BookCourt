59795a245c1f997ec00776281b852ca76747a90d
vini <101150191+vini-tn@users.noreply.github.com>
2024-04-13 18:06:22 +1200

    Vini: Created Models

--- BadmintonCourt/admin.py
+++ BadmintonCourt/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import court, MBooking, NBooking
 # Register your models here.
+admin.site.register(court)
+admin.site.register(MBooking)
+admin.site.register(NBooking)

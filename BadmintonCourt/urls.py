7347dfdbfa0812d821d484408829abf459fe4155
20220920 <20220920@mywhitecliffe.com>
2024-04-18 07:35:51 +0900

    forms

--- BadmintonCourt/urls.py
+++ BadmintonCourt/urls.py
@@ -4,5 +4,5 @@ from .import views
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('booking/', views.booking, name='booking'),
+    path('booking/', views.add_court, name='booking'),
 ]
--- BadmintonCourt/views.py
+++ BadmintonCourt/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import TemplateView
+from .forms import CourtForm
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -7,3 +8,13 @@ class HomeView(TemplateView):
 def booking(request):
 
     return render(request, 'booking.html')
+
+def add_court(request):
+    if request.method =="POST":
+        form = CourtForm(request.POST)
+        if form.is_valid() :
+            form.save()
+            return redirect('home')
+    else:
+        form = CourtForm()
+    return render(request, 'booking.html',{'form':form})

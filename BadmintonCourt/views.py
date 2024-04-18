778598dd76cc982c427411a614ea99e27e3a766c
vini <101150191+vini-tn@users.noreply.github.com>
2024-04-19 03:02:31 +1200

    Modified useraccounts and others

--- BadmintonCourt/views.py
+++ BadmintonCourt/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import court, MBooking, NBooking
 # Create your views here.
@@ -17,6 +17,15 @@ class MBookView(TemplateView):
 class NBookView(TemplateView):
     template_name = 'nbookingForm.html'
 
-class UserAccountView(LoginRequiredMixin, TemplateView):
+class UserAccountView(LoginRequiredMixin, ListView):
+    model = MBooking
     template_name = 'useraccount.html'
     login_url = 'accounts/login/'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.groups.filter(name='Members').exists():
+            queryset = queryset.filter(user=self.request.user)
+        else:
+            queryset = queryset.all()
+        return queryset
